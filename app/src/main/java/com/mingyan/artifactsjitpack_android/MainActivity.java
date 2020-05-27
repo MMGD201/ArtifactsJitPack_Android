3e6fad864c60e02a2355ffd496ee76654e0714f9
mmgd201 <ijnijn006@yahoo.com>
2020-05-27 20:12:32 +0800

    some annotation_1.1

--- app/src/main/java/com/mingyan/artifactsjitpack_android/MainActivity.java
+++ app/src/main/java/com/mingyan/artifactsjitpack_android/MainActivity.java
@@ -1,5 +1,5 @@
 package com.mingyan.artifactsjitpack_android;
-/**202005 AndroidStudio3.5.3
+/**202005 在AndroidStudio3.5.3，使用https://jitpack.io/  publish application library。
  * uploading / publish artifacts to the JitPack, first time.
  * 1_create android app
  * 2_File > New > New Module > Android Library > input class / code finish
@@ -8,7 +8,7 @@ package com.mingyan.artifactsjitpack_android;
  * 5_view Artifacts's  URL from GitHub
  * 6_past at https://jitpack.io/ home page > press Look up Button, if no release.
  * 6_When view the artifacts's URL and release is 0 , press releases and press Create new release > 填上Tag version( 例 : 0.1.0 ) > 填上Release title( 例 : home work, test 1. ) > press Publish release ( 勾勾原本沒勾也不用勾)
- * 7_past at https://jitpack.io/ home page and press Look up Button again, we see Version 0.1.0
+ * 7_past at  https://jitpack.io/  home page and press Look up Button again, we see Version 0.1.0
  * 8_publish artifacts complete, close artifacts project.*/
 import androidx.appcompat.app.AppCompatActivity;
 
